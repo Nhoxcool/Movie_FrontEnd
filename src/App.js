c88c76f387bcc1acc31f603dfc7fa72cdb022624
Tú Trần <nhoxcool2002@gmail.com>
2023-11-04 20:05:06 +0700

    second_commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Navbar from './components/user/Navbar';
+import Signin from './components/auth/Signin';
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Navbar />
+      <Signin />
+    </>
+  );
 }
