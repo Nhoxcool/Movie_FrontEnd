2da26005845bbdf29d0dc8c4e10ca846a6ecdf35
Tú Trần <nhoxcool2002@gmail.com>
2023-11-12 20:43:32 +0700

    fourth_commit

--- src/components/auth/ConfirmPassword.jsx
+++ src/components/auth/ConfirmPassword.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import Container from '../Container';
+import Title from '../form/Title';
+import FormInput from '../form/FormInput';
+import Submit from '../form/Submit';
+import FormContainer from '../form/FormContainer';
+import { commonModalClass } from '../../utils/Theme';
 
 export default function ConfirmPassword() {
-  return <div>ConfirmPassword</div>;
+  return (
+    <FormContainer>
+      <Container>
+        <form className={commonModalClass + ' w-96'}>
+          <Title>Enter New Password</Title>
+          <FormInput label="New Password" placeholder="********" name="password" type="password" />
+          <FormInput label="Comfirm Password" placeholder="********" name="comfirmPassword" type="password" />
+          <Submit value="Comfirm Password" />
+        </form>
+      </Container>
+    </FormContainer>
+  );
 }
